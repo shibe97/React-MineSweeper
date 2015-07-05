99e5eaf0bea61472837567043ea9532e9933f5a6
Kazuki Shibata <k.shibata97@gmail.com>
2015-07-06 01:06:09 +0900

    modify size

--- js/components/MineSweeper.js
+++ js/components/MineSweeper.js
@@ -5,14 +5,31 @@ export default class MineSweeper extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            level: "easy",
             mineNum : 10,
             rowNum : 10,
             flagNum : 0,
-            openNum : 0
+            openNum : 0,
+            time : 0
         };
     }
     componentWillUpdate() {
         this.judge();
+    }
+    componentWillMount() {
+        this.intervals = [];
+    }
+    componentWillUnmount() {
+        this.intervals.map(clearInterval);
+    }
+    componentDidMount() {
+        this.setInterval(this.tick.bind(this), 1000);
+    }
+    setInterval() {
+        this.intervals.push(setInterval.apply(null, arguments));
+    }
+    tick() {
+        this.setState({time: this.state.time + 1});
     }
     judge() {
         if(this.state.mineNum + this.state.openNum >= this.state.rowNum * this.state.rowNum){
@@ -39,24 +56,27 @@ export default class MineSweeper extends React.Component {
         });
     }
     setEasy() {
-        this.setState({mineNum: 10, rowNum: 10, openNum: 0, flagNum: 0});
+        this.setState({level: "easy", mineNum: 10, rowNum: 10, openNum: 0, flagNum: 0});
     }
     setNormal() {
-        this.setState({mineNum: 10, rowNum: 20, openNum: 0, flagNum: 0});
+        this.setState({level: "normal", mineNum: 40, rowNum: 16, openNum: 0, flagNum: 0});
     }
     setHard() {
-        this.setState({mineNum: 10, rowNum: 30, openNum: 0, flagNum: 0});
+        this.setState({level: "hard", mineNum: 100, rowNum: 22, openNum: 0, flagNum: 0});
     }
     render() {
         return (
-            <div className="MineSweeper">
+            <div>
                 <div className="MineSweeper__level">
                     <label><input type="radio" name="level" onChange={this.setEasy.bind(this)} />easy</label>
                     <label><input type="radio" name="level" onChange={this.setNormal.bind(this)} />normal</label>
                     <label><input type="radio" name="level" onChange={this.setHard.bind(this)} />hard</label>
                 </div>
-                <p className="MineSweeper__openNum">open : {this.state.openNum}</p>
-                <Table mineNum={this.state.mineNum} rowNum={this.state.rowNum} judge={this.judge.bind(this)} addOpenNum={this.addOpenNum.bind(this)}/>
+                <div className={"MineSweeper " + this.state.level}>
+                    <span className="MineSweeper__openNum"> {this.state.openNum}</span>
+                    <span className="MineSweeper__time"> {this.state.time}</span>
+                    <Table mineNum={this.state.mineNum} rowNum={this.state.rowNum} judge={this.judge.bind(this)} addOpenNum={this.addOpenNum.bind(this)}/>
+                </div>
             </div>
         );
     }
